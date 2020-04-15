f7114273ae82682ee00a7785f66c07bba3ef9e15
Tomoms <fondatommaso2@gmail.com>
2020-04-15 18:05:33 +0200

    First implementation In a scenario where the program must shuffle a set of 10k words, and 9999 of them have already been picked and printed, the program will fail to complete the task until the expression
    rand() % words + 1
    
    evaluates to the index of the last, missing word.
    I am afraid this, in the "worst case" scenario, can take quite a lot of loop cycles.

--- src/esercizio-C-2020-04-07.c
+++ src/esercizio-C-2020-04-07.c
@@ -1,8 +1,34 @@
-/*
- * esercizio-C-2020-04-07.c
- *
- *  Created on: Apr 2, 2020
- *      Author: marco
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
+// Words are expected to be passed as command line arguments!
+int main(int argc, char *argv[])
+{
+	int words = argc - 1;
+	time_t t = time(NULL);
+	srand(t);
+	bool *used = calloc(words, sizeof(bool));
+	if (used == NULL) {
+		perror("calloc() failed. Quitting.");
+		exit(EXIT_FAILURE);
+	}
 
+	int count = 0;
+	while (count < words) {
+		int rnd = rand() % words + 1;
+		if (used[rnd])
+			continue;
+		else
+			used[rnd] = 1;
+
+		printf(argv[rnd]);
+		if (count != words - 1)
+			printf(" ");
+		count++;
+	}
+
+	free(used);
+	return 0;
+}
