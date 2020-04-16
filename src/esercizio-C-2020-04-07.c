8966fb0cfa50ad50d5d1ddde2490ae5769cc075d
Tomoms <fondatommaso2@gmail.com>
2020-04-16 13:49:38 +0200

    Second implementation Each word is now removed from the array of words when it is printed.

--- src/esercizio-C-2020-04-07.c
+++ src/esercizio-C-2020-04-07.c
@@ -3,32 +3,36 @@
 #include <stdbool.h>
 #include <time.h>
 
+char** array_remove(char **arr, int len, int index)
+{
+	int offset = 0;
+	len--;
+	char **new_arr = calloc(len, sizeof(char*));
+	for (int i = 0; i < len; i++) {
+		if (i == index)
+			offset = 1;
+		new_arr[i] = arr[i + offset];
+	}
+	return new_arr;
+}
+
 // Words are expected to be passed as command line arguments!
 int main(int argc, char *argv[])
 {
 	int words = argc - 1;
 	time_t t = time(NULL);
 	srand(t);
-	bool *used = calloc(words, sizeof(bool));
-	if (used == NULL) {
-		perror("calloc() failed. Quitting.");
-		exit(EXIT_FAILURE);
-	}
 
-	int count = 0;
-	while (count < words) {
+	char **new_arr = argv;
+	while (words) {
 		int rnd = rand() % words + 1;
-		if (used[rnd])
-			continue;
-		else
-			used[rnd] = 1;
-
-		printf(argv[rnd]);
-		if (count != words - 1)
+		printf(new_arr[rnd]);
+		if (words > 1)
 			printf(" ");
-		count++;
+		new_arr = array_remove(new_arr, words + 1, rnd);
+		words--;
 	}
 
-	free(used);
+	free(new_arr);
 	return 0;
 }
